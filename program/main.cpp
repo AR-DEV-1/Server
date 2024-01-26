014c5bc8ce2d6f5284fe8c0f7f5ee671082f51ea
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2024-01-26 14:07:12 +0500

    Create main.cpp

---
+++ program/main.cpp
@@ -0,0 +1,24 @@
+// main.cpp
+
+#include "server.hpp"
+
+int main() {
+    const int PORT = 8080;
+    Server server;
+    int client_socket;
+
+    init_server(&server, PORT);
+    start_listening(&server);
+
+    while (1) {
+        printf("Waiting for connections...\n");
+        client_socket = accept_connection(&server);
+        printf("Connection accepted\n");
+
+        handle_client(client_socket);
+    }
+
+    close_server(&server);
+
+    return 0;
+}
